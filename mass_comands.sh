5dae5e9396a74085d2669f501aa87d5bf9773d03
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2023-10-16 07:13:22 +0100

    grabbing things from mass

---
+++ mass_comands.sh
@@ -0,0 +1,25 @@
+
+
+
+cd /net/spice/scratch/dkelley/future_ffdi/data/
+cd ffdi_output/rcp2_6_joined
+pwd
+moo get moose:/adhoc/users/inika.taylor/fire/rcp2_6_joined/* .
+cd ../rcp8_5_joined
+moo get moose:/adhoc/users/inika.taylor/fire/rcp8_5_joined/* .
+
+cd /scratch/dkelley/future_ffdi/data/threshold_exceedance/
+cd rcp2_6
+moo get moose:/adhoc/users/inika.taylor/fire/threshold_exceedance/rcp2_6/* .
+cd ../rcp8_5
+moo get moose:/adhoc/users/inika.taylor/fire/threshold_exceedance/rcp8_5/* .
+
+/net/spice/scratch/dkelley/future_ffdi/data/GlobalWarmingLevels/
+cd rcp2_6
+moo get moose:/adhoc/users/inika.taylor/fire/GlobalWarmingLevels/rcp2_6/* .
+cd ../rcp8_5
+moo get moose:/adhoc/users/inika.taylor/fire/GlobalWarmingLevels/rcp8_5/* .
+
+#cd GlobalWarmingLevels
+#moo ls moose:/adhoc/users/inika.taylor/fire/
+
